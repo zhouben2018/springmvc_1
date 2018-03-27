0f0798160fac18b277c97ffa74553a7ab5f5cfb9
zhouben2018 <zhouben@souche.com>
2018-03-27 16:26:41 +0800

    !

--- src/main/java/com/zben/test/springmvc/servlet/DispatcherServlet.java
+++ src/main/java/com/zben/test/springmvc/servlet/DispatcherServlet.java
@@ -173,7 +173,7 @@ public class DispatcherServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doGet(req, resp);
+        doPost(req, resp);
     }
 
     @Override
